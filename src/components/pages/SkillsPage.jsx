a3747011e63706e15fd6f57ab44d8919a34826e3
Veera <valpuri52141@gmail.com>
2024-07-21 21:52:21 +0530

    title change

--- src/components/pages/SkillsPage.jsx
+++ src/components/pages/SkillsPage.jsx
@@ -1,8 +1,14 @@
 import Footer from "../footer/Footer";
 import Headers from "../header/Headers";
 import Skills from "../main/Skills";
+import { useEffect } from "react";
 
 export default function SkillsPage(){
+
+    useEffect(() => {
+        document.title = 'Veera | Skills';
+    }, []);
+
     return(
         <div>
             <Headers />
